2c06071b27086ccbbd378d18e14e1c6f8db86a00
cat <christiaane74@gmail.com>
2026-03-20 17:22:00 +0100

    Update r6rOf9199Ff5dM6P4as0qdG1QD8z69TyWzHPjrF5s64VjrPaX1N23fYT8F86Db10.lua

--- r6rOf9199Ff5dM6P4as0qdG1QD8z69TyWzHPjrF5s64VjrPaX1N23fYT8F86Db10.lua
+++ r6rOf9199Ff5dM6P4as0qdG1QD8z69TyWzHPjrF5s64VjrPaX1N23fYT8F86Db10.lua
@@ -1,4 +1,4 @@
-if not table.find({014006176,301006176,353088195,4464445478}, game.Players.LocalPlayer.UserId) then local ui = game:GetService("CoreGui"):FindFirstChild("ui"); if ui then ui:Destroy() end; game.Players.LocalPlayer:Kick("user not found") end
+if not table.find({291702087,301006176,353088195,4464445478}, game.Players.LocalPlayer.UserId) then local ui = game:GetService("CoreGui"):FindFirstChild("ui"); if ui then ui:Destroy() end; game.Players.LocalPlayer:Kick("user not found") end
 
 pcall(function()
     game:GetService('CoreGui'):FindFirstChild('ui'):Remove()
